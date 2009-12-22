8f5c01bdfedb87626f82ebbd9b34ac2c92c41072
jorahood <jorahood@5cd652fc-3c8e-420e-a729-68eecf2ccb59>
2009-12-22 14:49:33 +0000

    switching to mysql for production app. Remember, for multistaging, deprec now requires a database.yml file in the subdirectory for each stage. E.g., I've got a config/staging/database.yml and config/prod/database.yml.
    git-svn-id: svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/kbpeer@20718 5cd652fc-3c8e-420e-a729-68eecf2ccb59

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :stages, %w(staging prod)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
-set :database_yml_in_scm, true
+set :database_yml_in_scm, false
 set :application, "kbpeer"
 set :repository, 'svn+ssh://poblano.uits.indiana.edu/srv/svn/kb-support/trunk/kbpeer'
 set :deploy_to, "/opt/apps/#{application}"
@@ -16,7 +16,7 @@ set :deploy_to, "/opt/apps/#{application}"
 set :ruby_vm_type,      :ree        # :ree, :mri
 set :web_server_type,   :apache     # :apache, :nginx
 set :app_server_type,   :passenger  # :passenger, :mongrel
-set :db_server_type,    :sqlite      # :mysql, :postgresql, :sqlite
+set :db_server_type,    :mysql      # :mysql, :postgresql, :sqlite
 
 # set :packages_for_project, %w(libmagick9-dev imagemagick libfreeimage3) # list of packages to be installed
 # set :gems_for_project, %w(rmagick mini_magick image_science) # list of gems to be installed
